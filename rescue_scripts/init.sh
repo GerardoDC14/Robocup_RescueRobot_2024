b86136b2829f99b8559375e5339c61de14b84a29
gerardo <gerardodelcid16@gmail.com>
2024-03-18 07:35:28 -0600

    Init. Scripts

--- rescue_scripts/init.sh
+++ rescue_scripts/init.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-# Source the ROS environment
+# Source env.
 source /opt/ros/noetic/setup.bash
 source /home/gerardo/rescue_ws/devel/setup.bash
 
 sudo chmod 666 /dev/ttyUSB0
 
-# Launch RPLeader ROS view
+# Launch rplidar 
 gnome-terminal -- bash -c "roslaunch rplidar_ros rplidar.launch; exec bash"
 
-# Wait a bit for the first launch to initialize
 sleep 5
 
-# Launch Hector SLAM tutorial
+# Launch Hector SlAM
 gnome-terminal -- bash -c "roslaunch hector_slam_launch tutorial.launch; exec bash"
 
 
